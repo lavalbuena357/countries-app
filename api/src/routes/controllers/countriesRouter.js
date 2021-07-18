d9cf7af3525f5a544f6bf478f401543facf82ec8
albertvr357 <cuervo357@gmail.com>
2021-07-18 01:17:35 -0500

    populate db

---
+++ api/src/routes/controllers/countriesRouter.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { Country } = require('../../db');
+const axios = require('axios');
+const { Sequelize } = require('sequelize');
+const router = express.Router();
+
+//Middleware
+router.use(express.json());
+
+//Middleaware populate DB
+router.use(async (req, res, next) => {
+  try {
+    const count = await Country.count()
+      if(!count) {
+        const api = await axios.get('https://restcountries.eu/rest/v2/all');
+        await api.data.forEach(el => Country.findOrCreate({
+          where: {
+            id: el.alpha3Code,
+            name: el.name,
+            flag: el.flag,
+            continent: el.region,
+            capital: el.capital,
+            subregion: el.subregion,
+            area: el.area,
+            population: el.population
+          }
+        }))
+      }; next();
+  } catch(err) {console.log(err)}
+})
+
+module.exports = router;
